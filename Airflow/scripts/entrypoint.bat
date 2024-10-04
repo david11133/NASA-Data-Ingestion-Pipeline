5b7697f3d9a2460e1e668a45145d91aa677a0956
david11133 <davidnady4yad@gmail.com>
2024-10-04 12:16:04 +0300

    update to match windows

--- Airflow/scripts/entrypoint.bat
+++ Airflow/scripts/entrypoint.bat
@@ -2,22 +2,28 @@
 setlocal
 
 REM Install requirements if the requirements.txt file exists
-if exist "C:\\opt\airflow\\requirements.txt" (
+if exist "C:\opt\airflow\requirements.txt" (
+    echo Installing requirements...
     python -m pip install --upgrade pip
-    pip install --user -r C:\\opt\\airflow\\requirements.txt
+    pip install --user -r C:\opt\airflow\requirements.txt
 )
 
 REM Initialize the Airflow database if it doesn't exist
 if not exist "C:\opt\airflow\airflow.db" (
+    echo Initializing Airflow database...
     airflow db init
     airflow users create --username admin --firstname admin --lastname admin --role Admin --email admin@example.com --password admin
 )
 
 REM Upgrade the database schema
+echo Upgrading Airflow database schema...
 airflow db upgrade
 
 REM Start the Airflow web server and scheduler
+echo Starting Airflow web server...
 start airflow webserver
+
+echo Starting Airflow scheduler...
 start airflow scheduler
 
 endlocal
