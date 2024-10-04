74db0ee2a58a539227a2e1c7947bc749ece1a726
david11133 <davidnady4yad@gmail.com>
2024-10-04 12:45:56 +0300

    connect to cassandra

--- Spark/kafka_consumer_spark.py
+++ Spark/kafka_consumer_spark.py
@@ -1,65 +1,127 @@
-import os
 import logging
+from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, json_tuple
-from pyspark.sql.types import StructType, StructField, StringType, BooleanType
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType, BooleanType, FloatType
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
-def main():
-    # Create Spark session
-    spark = SparkSession.builder \
-        .appName("Kafka Consumer Spark") \
-        .getOrCreate()
+def create_keyspace(session):
+    session.execute("""
+        CREATE KEYSPACE IF NOT EXISTS neo_data
+        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
+    """)
+    logging.info("Keyspace created successfully!")
 
-    # Load Kafka parameters from environment variables
-    kafka_broker = os.getenv("KAFKA_BROKER", "localhost:9092")  # Fallback to localhost if not set
-    kafka_topic = os.getenv("KAFKA_TOPIC", "your_kafka_topic")  # Fallback to a default topic
+def create_table(session):
+    session.execute("""
+    CREATE TABLE IF NOT EXISTS neo_data.near_earth_objects (
+        id UUID PRIMARY KEY,
+        date TEXT,
+        name TEXT,
+        estimated_diameter_min FLOAT,
+        estimated_diameter_max FLOAT,
+        is_potentially_hazardous BOOLEAN,
+        close_approach_date TEXT,
+        relative_velocity FLOAT,
+        miss_distance FLOAT
+    );
+    """)
+    logging.info("Table created successfully!")
 
-    # Read data from Kafka
-    df = spark.readStream \
-        .format("kafka") \
-        .option("kafka.bootstrap.servers", kafka_broker) \
-        .option("subscribe", kafka_topic) \
-        .load()
+def insert_data(session, **kwargs):
+    logging.info("Inserting data...")
+    try:
+        session.execute("""
+            INSERT INTO neo_data.near_earth_objects(id, date, name, estimated_diameter_min, 
+                estimated_diameter_max, is_potentially_hazardous, close_approach_date, 
+                relative_velocity, miss_distance)
+                VALUES (uuid(), %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (kwargs['date'], kwargs['name'], kwargs['estimated_diameter_min'],
+              kwargs['estimated_diameter_max'], kwargs['is_potentially_hazardous'],
+              kwargs['close_approach_date'], kwargs['relative_velocity'], kwargs['miss_distance']))
+        logging.info(f"Data inserted for {kwargs['name']} on {kwargs['date']}")
+    except Exception as e:
+        logging.error(f'Could not insert data due to: {e}')
 
-    # Deserialize JSON data from Kafka
-    value_df = df.selectExpr("CAST(value AS STRING)")
+def create_spark_connection():
+    s_conn = None
+    try:
+        s_conn = SparkSession.builder \
+            .appName('NEODataStreaming') \
+            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.13:3.4.1," 
+                                           "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1") \
+            .config('spark.cassandra.connection.host', 'localhost') \
+            .getOrCreate()
+        s_conn.sparkContext.setLogLevel("ERROR")
+        logging.info("Spark connection created successfully!")
+    except Exception as e:
+        logging.error(f"Couldn't create the Spark session due to: {e}")
+    return s_conn
 
-    # Define schema for the expected data
+def connect_to_kafka(spark_conn):
+    spark_df = None
+    try:
+        spark_df = spark_conn.readStream \
+            .format('kafka') \
+            .option('kafka.bootstrap.servers', 'localhost:9092') \
+            .option('subscribe', 'nasa_neo_data') \
+            .option('startingOffsets', 'earliest') \
+            .load()
+        logging.info("Kafka DataFrame created successfully")
+    except Exception as e:
+        logging.warning(f"Kafka DataFrame could not be created because: {e}")
+    return spark_df
+
+def create_cassandra_connection():
+    try:
+        cluster = Cluster(['localhost'])
+        return cluster.connect()
+    except Exception as e:
+        logging.error(f"Could not create Cassandra connection due to: {e}")
+        return None
+
+def create_selection_df_from_kafka(spark_df):
     schema = StructType([
-        StructField("Date", StringType(), True),
-        StructField("ID", StringType(), True),
-        StructField("Name", StringType(), True),
-        StructField("Estimated Diameter (km)", StringType(), True),
-        StructField("Is Potentially Hazardous", BooleanType(), True),
-        StructField("Close Approach Date", StringType(), True),
-        StructField("Relative Velocity (km/s)", StringType(), True),
-        StructField("Miss Distance (km)", StringType(), True)
+        StructField("date", StringType(), False),
+        StructField("name", StringType(), False),
+        StructField("estimated_diameter_min", FloatType(), False),
+        StructField("estimated_diameter_max", FloatType(), False),
+        StructField("is_potentially_hazardous", BooleanType(), False),
+        StructField("close_approach_date", StringType(), False),
+        StructField("relative_velocity", FloatType(), False),
+        StructField("miss_distance", FloatType(), False)
     ])
-
-    # Process the JSON data (assuming the incoming data is JSON formatted)
-    processed_df = value_df.select(
-        json_tuple("value", "Date", "ID", "Name", "Estimated Diameter (km)", "Is Potentially Hazardous", 
-                   "Close Approach Date", "Relative Velocity (km/s)", "Miss Distance (km)")
-    ).toDF("Date", "ID", "Name", "Estimated Diameter (km)", "Is Potentially Hazardous", 
-           "Close Approach Date", "Relative Velocity (km/s)", "Miss Distance (km)")
-
-    # Cast the "Is Potentially Hazardous" field to Boolean
-    processed_df = processed_df.withColumn("Is Potentially Hazardous", col("Is Potentially Hazardous").cast(BooleanType()))
-
-    # Example transformation: Filter hazardous asteroids
-    hazardous_df = processed_df.filter(col("Is Potentially Hazardous") == True)
-
-    # Write the output to the console (for testing)
-    query = hazardous_df.writeStream \
-        .outputMode("append") \
-        .format("console") \
-        .start()
-
-    logging.info(f"Listening for messages on topic: {kafka_topic} from broker: {kafka_broker}")
-    query.awaitTermination()
+    
+    sel = spark_df.selectExpr("CAST(value AS STRING)") \
+        .select(from_json(col('value'), schema).alias('data')).select("data.*")
+    
+    logging.info("Selection DataFrame created successfully.")
+    return sel
 
 if __name__ == "__main__":
-    main()
+    # Create Spark connection
+    spark_conn = create_spark_connection()
+
+    if spark_conn is not None:
+        # Connect to Kafka with Spark connection
+        spark_df = connect_to_kafka(spark_conn)
+        selection_df = create_selection_df_from_kafka(spark_df)
+        
+        # Create Cassandra connection
+        session = create_cassandra_connection()
+
+        if session is not None:
+            create_keyspace(session)
+            create_table(session)
+
+            logging.info("Streaming is being started...")
+
+            # Write the DataFrame to Cassandra
+            streaming_query = (selection_df.writeStream
+                               .foreachBatch(lambda batch_df, _: batch_df.foreach(lambda row: insert_data(session, **row.asDict())))
+                               .outputMode("update")
+                               .start())
+
+            streaming_query.awaitTermination()
